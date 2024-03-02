f05d47bece0cb7232d8fe56fc47fccc03cb192cf
Jamie Jarme <140039543+jamienewtron@users.noreply.github.com>
2024-03-02 09:24:36 +0800

    Update concatenate_results.py

--- src/kpdetector/concatenate_results.py
+++ src/kpdetector/concatenate_results.py
@@ -5,7 +5,7 @@ config = Config()
 
 dfs = []
 for cloth in ['blouse', 'skirt', 'outwear', 'dress', 'trousers']:
-    df = pd.read_csv(config.proj_path + 'predictions/' + cloth + '.csv')
+    df = pd.read_csv(config.proj_path + 'kp_predictions/' + cloth + '.csv')
     dfs.append(df)
 res_df = pd.concat(dfs)
 res_df.to_csv(config.proj_path +'kp_predictions/result.csv', index=False)
